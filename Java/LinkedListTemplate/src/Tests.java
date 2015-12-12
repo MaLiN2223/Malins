0b25eea0aee068dc17d3049d74fe912c3a12a0d4
MaLiN2223 <szyszka45@gmail.com>
2015-12-12 14:03:31 +0100

    Change clas Name + add Node

--- Java/LinkedListTemplate/src/Tests.java
+++ Java/LinkedListTemplate/src/Tests.java
@@ -8,18 +8,15 @@ public class Tests {
 	public void AdditionTest(){
 		final int size = 30;
 		TestObject[] objects = new TestObject[size];
-		MyListImplementation<TestObject> list = new MyListImplementation<>();
+		DoubleLinkedList<TestObject> list = new DoubleLinkedList<>();
 		for(int i =0;i<size;++i){
 			list.add(objects[i]);
-		}
-		for(int i =0;i<size;++i){
-			
 		} 
 	}@Test
 	public void AdditionTest2(){
 		final int size = 30;
 		TestObject[] objects = new TestObject[size];
-		MyListImplementation<TestObject> list = new MyListImplementation<>();
+		DoubleLinkedList<TestObject> list = new DoubleLinkedList<>();
 		for(int i =0;i<size;++i){
 			list.add(0,objects[i]);
 		}
