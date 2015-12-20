4a3ed592196a696fe49cae71d590afef3e2c5605
MaLiN2223 <szyszka45@gmail.com>
2015-12-20 02:42:23 +0100

    remove unused

--- DataStructures/Graphs/Interfaces/IDirectedGraph.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DataStructures.Graphs.Interfaces
-{
-    internal interface IDirectedGraph<T1, T2> : IGraph<T1> where T2 : IComparable<T2>
-    {
-    }
-}
--- DataStructures/Graphs/Interfaces/IWeightedEdge.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DataStructures.Graphs.Interfaces
-{
-    internal interface IWeightedEdge<T0, out T1> where T1 : IComparable<T1>, IEdge<T0>
-    {
-        T1 Weight { get; }
-    }
-}
